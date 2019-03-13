e888075da60e499d442ae04b313046039c537e38
DESKTOP-7BTGJ2T\Horger <mh3294@drexel.edu>
2019-03-13 17:44:33 -0400

    Update CRON Submit Button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import { Select, FormControl, FormHelperText, TextField, MenuItem, InputLabel, Input } from '@material-ui/core';
+import { FormControl, TextField, Button} from '@material-ui/core';
 
 export class Dashboard extends React.Component {
 
@@ -14,13 +14,18 @@ export class Dashboard extends React.Component {
     };
   }
 
-  handleChange(){
-    this.setState({ [event.target.name]: event.target.value });
+  validate(){
+    axios.get('http://www3.septa.org/hackathon/TrainView/')
+        .then(res => {
+            this.setState({
+                data: res.data
+            });
+        });
   };
 
   render(){
     return(
-      <div id="wrapper">
+      <div id="header-wrapper">
         <h1> CRON Expression Generator </h1>
           <h3> A simple tool that helps you build and get familar with common CRON expressions. <br></br>
           There are better generators out there, but this is meant for beginners / reference. Eventaully, you'll build your own! </h3>
@@ -51,15 +56,19 @@ export class CRONForm extends React.Component {
 
   render(){
     return(
-      <form style={{display: 'flex', flexWrap: 'wrap'}} autoComplete="off">
-        <CROMTextField id="seconds" label="Seconds" defaultValue="*" margin="normal" variant="filled"/>
-        <CROMTextField id="minutes" label="Minutes" defaultValue="*" margin="normal" variant="filled"/>
-        <CROMTextField id="hours" label="Hours" defaultValue="*" margin="normal" variant="filled"/>
-        <CROMTextField id="dayMon" label="Day of Month" defaultValue="*" margin="normal" variant="filled"/>
-        <CROMTextField id="month" label="Month" defaultValue="*" margin="normal" variant="filled"/>
-        <CROMTextField id="dayWeek" label="Day of Week" defaultValue="?" margin="normal" variant="filled"/>
-        <CROMTextField id="year" label="Year" defaultValue="*" margin="normal" variant="filled"/>
-      </form>
+      <div id="form-wrapper">
+        <form style={{display: 'flex', flexWrap: 'wrap'}} autoComplete="off">
+          <CROMTextField id="seconds" label="Seconds" defaultValue="*" margin="normal" variant="filled"></CROMTextField>
+          <CROMTextField id="minutes" label="Minutes" defaultValue="*" margin="normal" variant="filled"></CROMTextField>
+          <CROMTextField id="hours" label="Hours" defaultValue="*" margin="normal" variant="filled"></CROMTextField>
+          <CROMTextField id="dayMon" label="Day of Month" defaultValue="*" margin="normal" variant="filled"></CROMTextField>
+          <CROMTextField id="month" label="Month" defaultValue="*" margin="normal" variant="filled"></CROMTextField>
+          <CROMTextField id="dayWeek" label="Day of Week" defaultValue="?" margin="normal" variant="filled"></CROMTextField>
+          <CROMTextField id="year" label="Year" defaultValue="*" margin="normal" variant="filled"></CROMTextField>
+        </form>
+        <CROMButton id="submit" onClick="">Submit
+        </CROMButton>
+      </div>
     );
   }
 }
@@ -69,6 +78,17 @@ export class CROMTextField extends React.Component {
     const { id, label, defaultValue, margin, variant } = this.props;
     return(
       <TextField id={id} label={label} defaultValue={defaultValue} margin={margin} variant={variant}></TextField>
+    )
+  }
+}
+
+export class CROMButton extends React.Component {
+  render(){
+    const { children, id, onClick } = this.props;
+    return(
+      <Button id={id} onClick={onClick} variant="contained" color="primary">
+      {children}
+      </Button>
     )
   }
 }
