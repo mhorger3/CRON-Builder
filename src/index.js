0ba920384f048b5aeb0ac87b671f6e805e9654f7
DESKTOP-7BTGJ2T\Horger <mh3294@drexel.edu>
2019-03-13 16:13:49 -0400

    Add form components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Select, FormControl, FormHelperText } from '@material-ui/core';
+import { Select, FormControl, FormHelperText, MenuItem, InputLabel, Input } from '@material-ui/core';
+
+export class Dashboard extends Compenent {
+
+  state = {
+    age: '',
+    name: 'hai',
+    labelWidth: 0,
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render(){
+    const styles = {
+      root: {
+        display: 'flex',
+        flexwrap: 'wrap',
+      },
+    };
+
+    return(
+      <Form style={styles.root} autoComplete="off">
+        <FormControl>
+          <InputLabel htmlFor="age-simple">Day</InputLabel>
+          <Select value={this.state.age} onChange={this.handleChange} inputProps={{
+            name: 'age',
+            id: 'age-simple'
+          }}>
+          <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+        </Form>
+    );
+
+
+  }
+}
